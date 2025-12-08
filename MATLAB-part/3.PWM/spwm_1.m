d64583ef4730da651190f329ab49678d91972f27
Lvtou <ljk2789240298@163.com>
2025-12-08 15:37:33 +0800

    Modify project introduction

--- MATLAB-part/3.PWM/spwm_1.m
+++
@@ -1,7 +0,0 @@
-function [qU,qV,qW] = spwm_1(theta_U,theta_V,theta_W,a)
-    Up = -min(min(a*sin(theta_U),a*sin(theta_V)),a*sin(theta_W)) - 1;
-    qU = 1/2 + 1/2*(a*sin(theta_U) + Up);
-    qV = 1/2 + 1/2*(a*sin(theta_V) + Up);
-    qW = 1/2 + 1/2*(a*sin(theta_W) + Up);
-end
-    
--- MATLAB-part/4.PI_Param_Design/PR_PI/PR_controller.m
+++
@@ -1,7 +0,0 @@
-function [b0,b1,b2,a1,a2] = PR_controller(w0,Ki,wc,Ts)
-    b0 = 4*Ki*wc*Ts/(4+4*wc*Ts+w0^2*Ts^2);
-    b1 = 0;
-    b2 = -4*Ki*wc*Ts/(4+4*wc*Ts+w0^2*Ts^2);
-    a1 = (2*w0^2*Ts^2 - 8)/(4+4*wc*Ts+w0^2*Ts^2);
-    a2 = (4-4*wc*Ts + w0^2*Ts^2)/(4+4*wc*Ts+w0^2*Ts^2);
-end
